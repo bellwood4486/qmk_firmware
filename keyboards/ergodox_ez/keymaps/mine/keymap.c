d0c54c064ce5569dc97cb6ea08866dd615a34fef
SUZUKI Yoshiharu <Suzuki.Yoshiharu@fujixerox.co.jp>
2018-10-23 11:45:44 +0900

    feat: Change layout.

--- keyboards/ergodox_ez/keymaps/mine/keymap.c
+++ keyboards/ergodox_ez/keymaps/mine/keymap.c
@@ -31,18 +31,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |   =    |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |  Tab   |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  |   \    |
- * |--------+------+------+------+------+------|      |           | PstSc|------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------|  ESC |           | PstSc|------+------+------+------+------+--------|
  * |  Ctrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | Shift  |   Z  |   X  |   C  |   V  |   B  |  EN  |           |  JA  |   N  |   M  |   ,  |   .  |   /  | Shift  |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |   `  |  Alt | Left | right|                                       | Down |  Up  |   [  |   ]  |      |
+ *   |      |   `  |  Alt | Left | right|                                       |  Up  | Down |   [  |   ]  |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        |      | Ins  |       | Win  | Ctrl |
+ *                                        | Ctrl | Ins  |       | Win  | Ctrl |
  *                                 ,------|------|------|       |------+------+------.
  *                                 |      |      |      |       |      |      |      |
- *                                 |Enter |  Esc |------|       |------| Bkspc| Space|
+ *                                 |Enter |Delete|------|       |------| Bkspc| Space|
  *                                 |      |      | +L1  |       | +L1  |      |      |
  *                                 `--------------------'       `--------------------'
  */
@@ -51,19 +51,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
         KC_EQL,      KC_1,       KC_2,       KC_3,       KC_4,       KC_5,      _______,
-        KC_TAB,      KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,      _______,
+        KC_TAB,      KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,      KC_ESC,
         KC_LCTL,     KC_A,       KC_S,       KC_D,       KC_F,       KC_G,
         KC_LSFT,     KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,      CC_EN,
         XXXXXXX,     KC_GRV,     KC_LALT,    KC_LEFT,    KC_RGHT,
-                                                                     _______,   KC_INS,
+                                                                     KC_LCTL,   KC_INS,
                                                                                 _______,
-                                                         KC_ENTER,   KC_ESC,    MO(SYMB),
+                                                         KC_ENTER,   KC_DEL,    MO(SYMB),
         // right hand
         _______,     KC_6,       KC_7,       KC_8,        KC_9,       KC_0,       KC_MINS,
         KC_PSCR,     KC_Y,       KC_U,       KC_I,        KC_O,       KC_P,       KC_BSLS,
                      KC_H,       KC_J,       KC_K,        KC_L,       KC_SCLN,    KC_QUOT,
         CC_JA,       KC_N,       KC_M,       KC_COMM,     KC_DOT,     KC_SLSH,    KC_RSFT,
-                                 KC_DOWN,    KC_UP,       KC_LBRC,    KC_RBRC,    XXXXXXX,
+                                 KC_UP,      KC_DOWN,     KC_LBRC,    KC_RBRC,    XXXXXXX,
         KC_RWIN,     KC_LCTL,
         _______,
         MO(SYMB),    KC_BSPC,    KC_SPC
