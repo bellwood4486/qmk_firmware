ac0d25445b714732bba90abfdd908cfdd0ea9755
SUZUKI Yoshiharu <Suzuki.Yoshiharu@fujixerox.co.jp>
2018-11-26 11:04:35 +0900

    feat: ESCとWinを交換

--- keyboards/ergodox_ez/keymaps/mine/keymap.c
+++ keyboards/ergodox_ez/keymaps/mine/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *   |      |   `  |  Ins | Left | right|                                       |  Up  | Down |   [  |   ]  |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        | Alt  | Win  |       | ESC  | Alt  |
+ *                                        | Alt  | ESC  |       | Win  | Alt  |
  *                                 ,------|------|------|       |------+------+------.
  *                                 |      |      |      |       |      |      |      |
  *                                 |Bkspc |Delete|------|       |------| Enter| Space|
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,     KC_A,       KC_S,       KC_D,       KC_F,       KC_G,
         KC_LSFT,     KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,      CC_EN,
         XXXXXXX,     KC_GRV,     KC_INS,     KC_LEFT,    KC_RGHT,
-                                                                     KC_LALT,   KC_RWIN,
+                                                                     KC_LALT,   KC_ESC,
                                                                                 _______,
                                                          KC_BSPC,    KC_DEL,    CC_EN,
         // right hand
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                      KC_H,       KC_J,       KC_K,        KC_L,       KC_SCLN,    KC_QUOT,
         CC_JA,       KC_N,       KC_M,       KC_COMM,     KC_DOT,     KC_SLSH,    KC_RSFT,
                                  KC_UP,      KC_DOWN,     KC_LBRC,    KC_RBRC,    XXXXXXX,
-        KC_ESC,      KC_RALT,
+        KC_RWIN,     KC_RALT,
         _______,
         CC_JA,       KC_ENTER,   KC_SPC
     ),
